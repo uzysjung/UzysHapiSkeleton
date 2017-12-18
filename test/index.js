3c8eb8cac5791c5f1686e1a66b1638ddf25075ca
Uzys <uzysjung@gmail.com>
2017-12-18 23:31:24 +0900

    change test case

--- test/index.js
+++ test/index.js
@@ -2,52 +2,52 @@
  * Created by uzysjung on 15. 7. 9..
  */
 'use strict';
+
+const {  it , experiment , describe ,test } = exports.lab =  require('lab').script();
 const Code = require('code');
-const Lab = require('lab');
-
-const lab = exports.lab = Lab.script();
-
-const suite = lab.suite;
-const test = lab.test;
-const before = lab.before;
-const after = lab.after;
 const expect = Code.expect;
+//
+// const suite = lab.suite;
+// const test = lab.test;
+// const before = lab.before;
+// const after = lab.after;
+// const expect = lab.expect;
+
+
+experiment('math', () => {
+
+    test('returns true when 1 + 1 equals 2',  () => {
+
+        expect( 1 + 1 ).to.equal(2);
+    });
+});
+
+//BDD
+describe('math', () => {
+
+    it('returns true when 1 + 1 equals 2',  () => {
+
+        expect(1 + 1).to.equal(2);
+    });
+});
+
+
 const Server = require('../app');
-lab.test('main endpoint lists apis on the network', (done) => {
+
+
+
+test('main endpoint lists apis on the network', async () => {
 
     const options = {
         method: 'GET',
         url: '/api/3'
     };
 
-    Server.inject(options, (response) => {
+    const response = await Server.inject(options);
+    const result = response.result;
+    console.log('result:',result);
+    Code.expect(response.statusCode).to.equal(200);
+    Code.expect(result.name).to.be.instanceof(Array);
+    Code.expect(result.name).to.have.length(3);
 
-        const result = response.result;
-        console.log('result:',result);
-        Code.expect(response.statusCode).to.equal(200);
-        Code.expect(result.name).to.be.instanceof(Array);
-        Code.expect(result.name).to.have.length(3);
-
-        done();
-    });
-});
-
-
-lab.experiment('math', () => {
-
-    lab.test('returns true when 1 + 1 equals 2', (done) => {
-
-        Code.expect( 1 + 1 ).to.equal(2);
-        done();
-    });
-});
-
-//BDD
-lab.describe('math', () => {
-
-    lab.it('returns true when 1 + 1 equals 2', (done) => {
-
-        expect(1 + 1).to.equal(2);
-        done();
-    });
 });
